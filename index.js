11ac984af7e49bfed31a83a975ade091861598e9
farhanshahriyar <farhan.abir9999@gmail.com>
2024-02-22 22:49:35 +0600

    added cart api

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@hungrybox-db.oxcpesj.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri)
+// console.log(uri)
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -26,7 +26,30 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    // Send a ping to confirm a successful connection
+
+
+  // Establish connection for database and collections
+    const menuCollections = client.db("hungrybox-db").collection("menu");
+    const cartCollections = client.db("hungrybox-db").collection("cartItems");
+
+    // all menu items operations
+    app.get('/menu', async (req, res) => {
+      const result = await menuCollections.find({}).toArray();
+      res.send(result);
+    });
+
+
+    // all cart items operations
+
+    // 1. posting cart on db
+    app.post('/carts', async (req, res) => {
+      const cartItem = req.body;
+      const result = await cartCollections.insertOne(cartItem);
+      res.send(result);
+    });
+
+
+
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
