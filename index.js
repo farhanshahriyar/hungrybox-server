25f733d3ee0de407cdb3c9b9c9310acd90422925
farhanshahriyar <farhan.abir9999@gmail.com>
2024-02-29 22:29:40 +0600

    added update api on quantity functions

--- index.js
+++ index.js
@@ -71,6 +71,20 @@ async function run() {
       res.send(result);
     });
 
+    // 5. Update cart quantity
+    app.put('/carts/:id', async (req, res) => {
+      const id = req.params.id;
+      const {quantity} = req.body;
+      const filter = {_id: new ObjectId(id)};
+      const options = {upsert: true};
+      const updateDoc = {
+        $set: {
+          quantity: parseInt(quantity,10),
+        },
+      };
+      const result = await cartCollections.updateOne(filter, updateDoc, options);
+      // res.send(result);
+    });
 
 
 
